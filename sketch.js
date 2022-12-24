50d889db39b865e9a41327beb93ddd4eeb2bba92
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2022-12-24 20:12:59 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,11 +2,12 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-let engine;
-let world;
-var ball;
-var rect;
-var square;
+var engine;
+var world;
+//cuidado com palavras reservadas, existe o comando rect, por exemplo. Sugiro trocar o nome das variáveis para block1, block2 e block3
+var block1;
+var block2;
+var block3;
 
 function preload()
 {
@@ -14,47 +15,66 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	var canvas = createCanvas(800, 700);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
 	//Crie os Corpos Aqui.
-	var ball_options = {
+	
+	var plane_options = {
+      	  	isStatic: true
+        }
+	
+	plane = Bodies.rectangle(600,580,1200,2,plane_options);
+    	World.add(world,plane);
+	
+	var block1_options = {
 		restitution : 0.95,
-		frictionAir : 0.01
+		friction:0.02,
+      		frictionAir:0
 	  }
 	
-	  var rect_options = {
-		isStatic : true
+	var block2_options = {
+		restitution:0.7,
+      		friction:0.01,
+      		frictionAir:0.1
 	  }
-	  var square_options ={
-		  restitution : 0.85,
-	  }
-	  ball = Bodies.circle(100,10,20,ball_options);
-	  World.add(world,ball);
 	
-	  ground = Bodies.rectangle(200,390,400,20,rect_options);
-	  World.add(world,rect);
-	  ground = Bodies.rectangle(200,390,20,20,square_options);
-	  World.add(world,square);
+	var block3_options ={
+		restitution:0.01,
+      		friction:1,
+      		frictionAir:0.3
+	  }
+	
+	  block1 = Bodies.circle(200,10,10,block1_options);
+	  World.add(world,block1);
+	
+	 block2 = Bodies.rectangle(110,50,10,10,block2_options);
+    	 World.add(world,block2);
+    
+    	 block3 = Bodies.rectangle(350,50,10,10,block3_options);
+    	 World.add(world,block3); 
 
-	Engine.run(engine);
-    wall=Bodies.rectangle(300,200,200,20,rect_options);
-  World.add(world,wall);
+	//estilizando os corpos aqui
+    	fill("brown");
+    	rectMode(CENTER);
+    	ellipseMode(RADIUS);
 }
 
 
 function draw() {
-  rectMode(CENTER);
   Engine.update(engine);
   background(225);
-  ellipse(ball.position.x,ball.position.y,20);
-  rect(ground.position.x,rect.position.y,400,20);
-  square(ground.position.x,square.position.y,20,20);
-  drawSprites();
- 
+	
+  //criada forma para o plano e suporte
+  rect(plane.position.x,plane.position.y,1200);
+	
+  ellipse(block1.position.x,block1.position.y,30);
+  rect(block2.position.x,block2.position.y,50,50);
+  rect(block3.position.x,block3.position.y,100,50);
+   
 }
 
 
